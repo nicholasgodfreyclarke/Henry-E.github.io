7af857bcd65524126aa8ddf53eb8da2b1ea1bfd8
Nicholas Clarke <nick@nicholasgodfreyclarke.com>
2016-06-21 19:35:29 +0100

    Dynamic buttons and other features
    - Buttons are added by javascript
    - User can choose how many words in game (2, 4, 6)
    - Play again does not refresh page (or json ajax request)
    - Short description of how to play
    - Track win percentage across games in session

--- Word2VecWithFriends/script.js
+++ Word2VecWithFriends/script.js
@@ -1,7 +1,82 @@
 var numberSuccesses = 0;
+var numberFailues = 0;
 var gameOver = false;
-var wordData;
-var buttonIds = ["button1", "button2", "button3", "button4"];
+var wordJson;
+var buttonIds = ["button1", "button2", "button3", "button4", "button5", "button6"];
+var numberButtons;
+var numberOfWins = 0;
+var numberOfLosses = 0;
+
+$( document ).ready(function() {
+    getWordJson();
+    numberButtons = 4;
+    createGame();
+});
+
+function createGame() {
+
+    // Reset game
+    gameOver = false;
+    numberSuccesses = 0;
+    numberFailues = 0;
+    $('#buttonDiv').html('');
+    $("#gameOutcome").css("visibility",  "hidden");
+    $("#playAgainButton").css("visibility",  "hidden");
+    
+    createButtons(numberButtons);
+    updateButtonText(numberButtons, wordJson);
+}
+
+function getWordJson() {
+    // Need to set async to false
+    $.ajax({
+      dataType: "json",
+      url: "words.json",
+      data: "",
+      async: false,
+      success: function(data) {
+        wordJson = data;
+      }
+    });
+}
+
+function createButtons(numberButtons) {
+    
+    var buttonHtml = '';
+
+    for (i = 1; i < numberButtons/2 + 1; i++) {
+        buttonHtml = buttonHtml.concat('<button type="button" class="btn btn-primary choiceButton" id="button' + i + '" style="width:200px">' + i + '</button>');
+    }
+    buttonHtml = buttonHtml.concat('<br><br>');   
+    for (i = numberButtons/2 + 1; i < numberButtons + 1; i++) {
+        buttonHtml = buttonHtml.concat('<button type="button" class="btn btn-primary choiceButton" id="button' + i + '" style="width:200px">' + i + '</button>');         
+    }
+
+    $('#buttonDiv').append(buttonHtml);
+};
+
+function updateButtonText(numberButtons, json) {
+
+        if (numberButtons === 2) {
+            wordData = json["examplesOfTwo"] 
+        } else if (numberButtons === 4) {
+            wordData = json["examplesOfFour"]
+        } else if (numberButtons === 6) {
+            wordData = json["examplesOfSix"]
+        }
+
+        // Randomly select a set of words
+        wordData = wordData[Math.floor(wordData.length * Math.random())];
+
+        // shuffle negative and positive words
+        words = shuffle(wordData["positive words"].concat(wordData["negative words"]));
+        
+        // Add words to page
+        $("#clue").text("Clue: " + wordData["clue"]);
+        for (i = 0; i < numberButtons; i++) {
+            $("#" + buttonIds[i]).text(words[i]);
+        }
+}
 
 // http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffle(array) {
@@ -23,55 +98,66 @@ function shuffle(array) {
   return array;
 }
 
-$.getJSON("examplesOfFour.json", function(json) {
-    // Put logic in here as getJSON is async (or could call out to another function)
-
-    // Randomly select a set of words
-    wordData = json[Math.floor(json.length * Math.random())]
-
-    // shuffle negative and positive words
-    words = shuffle(wordData["positive words"].concat(wordData["negative words"]));
-    
-    // Add words to page
-    $("#clue").text("Clue: " + wordData["clue"]);
-    for (i = 0; i < 4; i++) {
-        $("#" + buttonIds[i]).text(words[i]);
-    }
-});
-
-
 // Button handler: set button colour after choice and check whether game over
-$( ".choiceButton" ).click(function() {
-    $(this).removeClass("btn-default");
-    
-    if ($.inArray($(this).text(), wordData["positive words"]) !== -1) {
-        $(this).addClass("btn-success");
-        numberSuccesses += 1;
-    } else {
-        // Reveal answers to buttons
-        for (i = 0; i < 4; i++) {
-            if ($.inArray($("#" + buttonIds[i]).text(), wordData["positive words"]) !== -1) {
-                $("#" + buttonIds[i]).addClass("btn-success");
-            } else {
-                $($("#" + buttonIds[i])).addClass("btn-danger"); 
-            }
+// Set handler to body so it can pick up buttons added dynamically by javascript
+$('body').on('click', '.choiceButton', function() {
+    if (gameOver === false) {
+        $(this).removeClass("btn-default");
+        
+        if ($.inArray($(this).text(), wordData["positive words"]) !== -1) {
+            $(this).addClass("btn-success");
+            numberSuccesses += 1;
+        } else {
+            $(this).addClass("btn-danger");
+            numberFailues += 1;
         };
 
-        // Game over - sorry dude
-        $("#gameOutcome").text("You have lost!");
-        $("#gameOutcome").css("visibility",  "visible");
-        gameOver = true;
-        $("#playAgainButton").css("visibility",  "visible");
-    };
-
-    if (numberSuccesses === 2 && gameOver === false) {
-        $("#gameOutcome").text("You have won!");
-        $("#gameOutcome").css("visibility",  "visible");
-        gameOver = true;
-        $("#playAgainButton").css("visibility",  "visible");
-    };
-
+        if (numberSuccesses === numberButtons/2) {
+            numberOfWins++;
+            $("#winPercentage").text("Win percentage: " + Math.floor(numberOfWins / (numberOfWins + numberOfLosses) * 100) + "%");
+            gameOver = true;
+            revealAnswers();
+            $("#gameOutcome").text("You have won!");
+            $("#gameOutcome").css("visibility",  "visible");
+            $("#playAgainButton").css("visibility",  "visible");
+        } else if (numberFailues === numberButtons/2) {
+            numberOfLosses++;
+            $("#winPercentage").text("Win percentage: " + Math.floor(numberOfWins / (numberOfWins + numberOfLosses) * 100) + "%");
+            gameOver = true;
+            revealAnswers();
+            $("#gameOutcome").text("You have lost!");
+            $("#gameOutcome").css("visibility",  "visible");
+            $("#playAgainButton").css("visibility",  "visible");
+        }
+    }
 })
 
+function revealAnswers() {
+    for (i = 0; i < numberButtons; i++) {
+        if ($.inArray($("#" + buttonIds[i]).text(), wordData["positive words"]) !== -1) {
+            $("#" + buttonIds[i]).addClass("btn-success");
+        } else {
+            $($("#" + buttonIds[i])).addClass("btn-danger"); 
+        }
+    }; 
+}
 
+$('body').on('click', '#playAgainButton', function() {
+    createGame();
+})
 
+$('body').on('click', '.numberOfWordsButton', function() {
+    var selection = $(this).text();
+
+    if (selection === '2 words') {
+        numberButtons = 2;
+        createGame();
+    } else if (selection === '4 words') {
+        numberButtons = 4;
+        createGame();
+    } else if (selection === '6 words') {
+        numberButtons = 6;
+        createGame();
+    }
+
+})
